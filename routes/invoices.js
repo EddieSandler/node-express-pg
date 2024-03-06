0e5972a21fd220b3bef23b362650542571957b1b
Eddie Sandler <ed.sandler@gmail.com>
2024-03-06 17:21:06 -0500

    get requests completed

--- routes/invoices.js
+++ routes/invoices.js
@@ -4,19 +4,31 @@ const router = new express.Router();
 const db = require("../db");
 
 router.get('/', async (req, res, next) => {
-
   try {
-    const results = await db.query(`SELECT comp_code,amt
+    const results = await db.query(`SELECT id,comp_code,amt
     FROM invoices
-    ORDER BY comp_code
+    ORDER BY id
     `);
-
     return res.json({invoices:results.rows})
   } catch (e) {
     return next(e);
   }
 })
+//GET /invoices/[id] : Returns obj on given invoice.
+router.get('/:id',async(req,res,next)=>{
+  try {
+      const { id } = req.params;
+      const results = await db.query('SELECT * FROM invoices JOIN companies on invoices.comp_code=companies.code WHERE id =$1 ', [id]);
+      if (results.rows.length === 0) {
+        throw new ExpressError(`Can't find invoice  with id of ${ id }, 404`);
+      }
+      return res.send({ code: results.rows[0] });
 
+
+  } catch (e){
+    return next(e)
+  }
+})
 
 
 
